194048452a5804fa698f10132cc350fe9016d330
s.kasireddy <sudarsana.kasireddy@latoken.com>
2021-02-28 10:46:53 -0500

    refactored and added Kafka consumer to read tradeNew messages

--- src/main/java/app/protobuf/messaging/AppProtobufMessagingApplication.java
+++ src/main/java/app/protobuf/messaging/AppProtobufMessagingApplication.java
@@ -1,24 +1,8 @@
 package app.protobuf.messaging;
 
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.ByteArrayDeserializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.listener.ContainerProperties;
-import org.springframework.stereotype.Component;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootApplication
 @EnableKafka
--- src/main/java/app/protobuf/messaging/consumer/AppConsumer.java
+++ src/main/java/app/protobuf/messaging/consumer/AppConsumer.java
@@ -2,6 +2,7 @@ package app.protobuf.messaging.consumer;
 
 import app.protobuf.messaging.grpc.util.ProtoUtil;
 import app.protobuf.messaging.model.HelloRequest;
+import app.protobuf.messaging.model.TradeRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
-public class GreetingConsumer {
+public class AppConsumer {
 
 
     @KafkaListener(
@@ -20,5 +21,14 @@ public class GreetingConsumer {
 
         HelloRequest request = ProtoUtil.parse(buffer, HelloRequest.class);
         log.info("received request ::"+request);
+    }
+
+    @KafkaListener(
+            topics = "trade_new",
+            containerFactory = "kafkaListenerContainerFactory"
+    )
+    public void tradeNewReceiver(@Payload byte[] buffer) {
+        TradeRequest tradeRequest = ProtoUtil.parse(buffer, TradeRequest.class);
+        log.info("tradeNew request received::"+tradeRequest);
     }
 }
